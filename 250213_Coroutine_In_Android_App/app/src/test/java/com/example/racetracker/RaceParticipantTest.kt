bdccbfaf6b96d2c5f413874fef0b13ae53a61fe7
sowjd <sowjd1225@gmail.com>
2025-02-13 14:58:01 +0900

    test: Game starts and finishes

--- 250213_Coroutine_In_Android_App/app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ 250213_Coroutine_In_Android_App/app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -16,6 +16,12 @@
 package com.example.racetracker
 
 import com.example.racetracker.ui.RaceParticipant
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class RaceParticipantTest {
     private val raceParticipant = RaceParticipant(
@@ -25,4 +31,24 @@ class RaceParticipantTest {
         initialProgress = 0,
         progressIncrement = 1
     )
+
+    // 레이스 시작 후 진행률 테스트
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        launch { raceParticipant.run() } // 새로운 코루틴에서 실행 (비동기 실행) => why?
+        // raceParticipant.run()에 delay()가 있는데 테스트가 delay() 만큼 기다려야하기 때문, advanceTimeBy()도 실행 안됨
+        advanceTimeBy(raceParticipant.progressDelayMillis) // progressDelayMillis 시간 만큼 시간을 앞으로 이동 (기다림 없음)
+        runCurrent() // 현재 대기중인 코루틴을 즉시 실행, advanceTimeBy()만으로 실행이 보장되지 않을 수도 있기 때문에 추가함.
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    // 레이스 완료 후 진행률 테스트
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.maxProgress * raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(100, raceParticipant.currentProgress)
+    }
 }
